6d2972aeb1742241498f6c3ffda62325d62df050
sqzaman <sqzaman@gmail.com>
2018-09-04 14:17:52 -0500

    Exercise 2 added

---
+++ Exercise-2/AMP2/src/main/java/mscs/mum/edu/AppEmpDept.java
@@ -0,0 +1,75 @@
+package mscs.mum.edu;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+public class AppEmpDept {
+	private static SessionFactory sessionFactory;
+
+	/* Reads hibernate.cfg.xml and prepares Hibernate for use */
+	protected static void setUp() throws Exception {
+		// A SessionFactory is set up once for an application!
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		try {
+			sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		} catch (Exception e) {
+			e.printStackTrace();
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+
+	protected static void tearDown() throws Exception {
+		if (sessionFactory != null) {
+			sessionFactory.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		setUp();
+		Session session = null;
+		Transaction tx = null;
+		try {
+
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
+
+			// retrieve all student
+
+
+			Department d1 = new Department("HR");
+			Department d2 = new Department("Sales");
+			Department d3 = new Department("Marketting");
+			session.persist(d1);
+			session.persist(d2);
+			session.persist(d3);
+			
+			Employee emp1 = new Employee("Syed Quamruzzaman", d1);
+			Employee emp2 = new Employee("Samiul Jahan", d2);
+			Employee emp3 = new Employee("Ali Yilmaz", d3);
+			session.persist(emp1);
+			session.persist(emp2);
+			session.persist(emp3);
+			
+			
+			tx.commit();
+		} catch (HibernateException e) {
+			tx.rollback();
+			e.printStackTrace();
+		} finally {
+			if (session != null)
+				session.close();
+		}
+
+
+		tearDown();
+	}
+
+}
---
+++ Exercise-2/AMP2/src/main/java/mscs/mum/edu/Employee.java
@@ -0,0 +1,55 @@
+package mscs.mum.edu;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue
+	@Column(name = "employee_number")
+	private long employeeNumber;
+	@Column(name = "employee_name")
+	private String name;
+	@ManyToOne
+	@JoinColumn(name = "department_id")
+	private Department department;
+	
+	public Employee() {
+	}
+	/**
+	 * @param name
+	 * @param department
+	 */
+	public Employee(String name, Department department) {
+		this.name = name;
+		this.department = department;
+	}
+	public long getEmployeeNumber() {
+		return employeeNumber;
+	}
+	public void setEmployeeNumber(long employeeNumber) {
+		this.employeeNumber = employeeNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	
+
+}
