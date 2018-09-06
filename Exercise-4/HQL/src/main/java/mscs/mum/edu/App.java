e93bc7933542ea07c33fa36e6d514c77092d4065
Shinokkk <yilmazali325@gmail.com>
2018-09-06 16:33:59 -0500

    update

---
+++ Exercise-4/HQL/src/main/java/mscs/mum/edu/App.java
@@ -0,0 +1,14 @@
+package mscs.mum.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
