569aa6c905a71811a786c42a44033adc9204664f
sqzaman <sqzaman@gmail.com>
2018-09-09 14:17:41 -0500

    exercise 5

---
+++ Exercise-5/HOP1/src/main/java/cs544/hop1/Owner.java
@@ -0,0 +1,59 @@
+package cs544.hop1;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+
+@Entity
+
+public class Owner {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	@OneToMany(cascade = { CascadeType.PERSIST }, fetch=FetchType.EAGER )
+	@JoinColumn(name = "clientid")
+	private List<Pet> pets;
+
+	public Owner() {
+	}
+
+	public Owner(String name) {
+		super();
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(List<Pet> pets) {
+		this.pets = pets;
+	}
+
+}
