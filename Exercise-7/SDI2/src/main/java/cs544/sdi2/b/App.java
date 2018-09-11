9c2aa45e17b2919e5755a2f47f472babddeb00d2
sqzaman <sqzaman@gmail.com>
2018-09-11 14:11:48 -0500

    updated

--- Exercise-7/SDI2/src/main/java/cs544/sdi2/b/App.java
+++ Exercise-7/SDI2/src/main/java/cs544/sdi2/b/App.java
@@ -3,14 +3,12 @@ package cs544.sdi2.b;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App 
-{
+public class App {
 	public static void main(String[] args) {
 		// IProductService productService = new ProductService();
-		ApplicationContext context = new  ClassPathXmlApplicationContext("springconfig.xml");
-				IProductService productService = context.getBean("productService", IProductService.class);
+		ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
+		IProductService productService = context.getBean("productService", IProductService.class);
 
-				/*
 		Product product1 = productService.getProduct(423);
 		if (product1 != null) {
 			System.out.println(product1.toString());
@@ -19,11 +17,10 @@ public class App
 		if (product2 != null) {
 			System.out.println(product2.toString());
 		}
-*/
-				
-				System.out.println("we have " + productService.getNumberInStock(423)
-				+ " product(s) with productNumber 423 in stock");
-				System.out.println("we have " + productService.getNumberInStock(239)
-				+ " product(s) with productNumber 239 in stock");
+
+		System.out.println(
+				"we have " + productService.getNumberInStock(423) + " product(s) with productNumber 423 in stock");
+		System.out.println(
+				"we have " + productService.getNumberInStock(239) + " product(s) with productNumber 239 in stock");
 	}
 }
