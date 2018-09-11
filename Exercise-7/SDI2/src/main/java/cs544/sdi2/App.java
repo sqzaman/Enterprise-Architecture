e47a99ee84c2547be8c90c83932355e5bf434955
sqzaman <sqzaman@gmail.com>
2018-09-11 13:54:32 -0500

    1st part of problem 2 is done

---
+++ Exercise-7/SDI2/src/main/java/cs544/sdi2/App.java
@@ -0,0 +1,23 @@
+package cs544.sdi2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+	public static void main(String[] args) {
+		// IProductService productService = new ProductService();
+		ApplicationContext context = new  ClassPathXmlApplicationContext("springconfig.xml");
+				IProductService productService = context.getBean("productService", IProductService.class);
+
+		Product product1 = productService.getProduct(423);
+		if (product1 != null) {
+			System.out.println(product1.toString());
+		}
+		Product product2 = productService.getProduct(239);
+		if (product2 != null) {
+			System.out.println(product2.toString());
+		}
+
+	}
+}
