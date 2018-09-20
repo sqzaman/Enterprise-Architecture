3560ecae7722ba9ec002909bb783adc95fc65bcf
sqzaman <sqzaman@gmail.com>
2018-09-20 15:42:55 -0500

    updated

--- Exercise-14/JMSReceiver/src/main/java/mum/edu/mscs/cs544/JMSMessageListener.java
+++ Exercise-14/JMSReceiver/src/main/java/mum/edu/mscs/cs544/JMSMessageListener.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class JMSMessageListener {	
 	
-	private static int result = 0;
+
 
 	public JMSMessageListener() {
 		System.out.println(".....listening jms messages......"); 
@@ -20,28 +20,10 @@ public class JMSMessageListener {
 	public void receiveMessage(final Calculate calcObj) {
 		System.out.println("Received" + calcObj);
 		System.out.println("===================================");
-		System.out.println(String.format("Previous value: %d, now got: %s, %d", result, calcObj.getOperator(), calcObj.getValue()));
-		System.out.println(String.format("Result after calculate: %d", calculateMe(calcObj)));
+		System.out.println(String.format("Previous value: %d, now got: %s, %d", Calculator.getResult(), calcObj.getOperator(), calcObj.getValue()));
+		System.out.println(String.format("Result after calculate: %d", Calculator.calculateMe(calcObj)));
 		System.out.println("===================================");
 	
 		
-	}
-	
-	static int calculateMe(Calculate calcObj) {
-		switch (calcObj.getOperator()) {
-		case '+':
-			result = result + calcObj.getValue();
-		case '-':
-			result = result - calcObj.getValue();
-		case '*':
-			result = result * calcObj.getValue();
-		case '/':
-			result = result / calcObj.getValue();
-		default:
-		
-		}
-		
-		return result;
-
 	}
 }
